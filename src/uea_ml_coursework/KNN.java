97e756b2d20d407fe6592dd6db03ee521c236ca9
Bijan Ghasemi Afshar <bijan.ghasemiafshar@gmail.com>
2019-04-10 23:35:24 +0100

    Initial Commit (Created KNN - Incomplete)

---
+++ src/uea_ml_coursework/KNN.java
@@ -0,0 +1,154 @@
+/*
+ * This is the implementation of the K-Nearest Neighbour classifier with 
+ * additional functionalities. It extends the AbstractClassifier class from
+ * the Weka machine learning package. This implementation assumes all attributes
+ * are real values.
+ */
+package uea_ml_coursework;
+
+import weka.classifiers.AbstractClassifier;
+import weka.core.Instance;
+import weka.core.Instances;
+
+/**
+ * 10/04/2019
+ * @author Bijan Ghasemi Afshar (100125463)
+ */
+public class KNN extends AbstractClassifier{
+
+    // Class properties
+    private Instances trainingData;
+    private int k;
+    private int[] votes;
+    
+    /**
+     * Constructor for initialising the KNN object.
+     */
+    public KNN(){
+        this.k = 1;
+    }
+    
+    /**
+     * Sets the K for KNN classifier.
+     * @param k The number of closest neighbours considered for classification.
+     */
+    public void setK(int k){
+        this.k = k;
+    }
+    
+    /**
+     * Builds the classifier by storing the training data.
+     * @param data The classified training data
+     * @throws Exception 
+     */
+    @Override
+    public void buildClassifier(Instances data) throws Exception {
+        trainingData = data;
+        votes = new int[trainingData.numClasses()];
+    }
+    
+    /**
+     * This function calculates the Euclidean distance between the attributes of
+     * a classified and an unclassified object and returns that value.
+     * @param data The classified training instance.
+     * @param object The unclassified instance.
+     * @return The Euclidean distance in a double format.
+     */
+    private double distance(Instance data, Instance object){
+        
+        int numOfAttr = data.numAttributes() - 1;
+        double difference = 0.0, diffSqr = 0.0, total = 0.0;
+        
+        for (int i = 0; i < numOfAttr; i++){
+            
+//            System.out.println("Classified: " + data.value(i) + "\nObject: "
+//             + object.value(i));
+            difference = object.value(i) - data.value(i);
+            diffSqr = Math.pow(difference, 2);
+            total += diffSqr;
+            
+        }
+//        System.out.println("Distance: " + total + "\n");
+        
+        return total;
+        
+    }
+    
+    /**
+     * Resets votes from previous classifications to avoid conflicts.
+     */
+    private void resetVotes(){
+        
+        for (int i = 0; i < votes.length; i++){
+            
+            votes[i] = 0;
+            
+        }
+        
+    }
+    
+    @Override
+    public double classifyInstance(Instance object){
+        
+        double closestMatch, eDistance = 0.0;
+        Instance[] closestInstances = new Instance[k];
+        Instances dataCopy = new Instances(trainingData);
+        int closestInstanceIndex = 0, classIndex = 0, numOfVotes = 0;
+        resetVotes();   // Reset votes from previous classification
+        
+        System.out.println("Number of K: " + closestInstances.length);
+        
+        // Go through all training data K times and choose smallest distance
+        for (int i = 0; i < closestInstances.length; i++){
+            
+            closestMatch = Double.MAX_VALUE;
+            
+            for (int j = 0; j < dataCopy.numInstances(); j++){
+                
+                eDistance = distance(dataCopy.instance(j), object);
+//                System.out.println(eDistance);
+                if (eDistance < closestMatch){
+                    closestMatch = eDistance;
+                    closestInstances[i] = dataCopy.instance(j);
+                    closestInstanceIndex = j;
+                }
+
+            }
+            dataCopy.delete(closestInstanceIndex);
+            System.out.println("Closest Match: " + closestMatch);
+            classIndex = (int)(closestInstances[i].classValue());
+            votes[classIndex]++;
+        }
+        
+        // Count the votes
+        classIndex = 0;
+        for (int i = 0; i < votes.length; i++){
+            System.out.println(i + " " + votes[i]);
+            if (numOfVotes < votes[i]){
+                numOfVotes = votes[i];
+                classIndex = i;
+            }
+        }
+        System.out.println(classIndex);
+        
+        return (double)classIndex;
+    }
+    
+    @Override
+    public double[] distributionForInstance(Instance object){
+        
+        double[] results = new double[trainingData.numClasses()];
+        
+        classifyInstance(object);
+        
+        for (int i = 0; i < results.length; i++){
+            
+            System.out.println(votes[i] + " " + k);
+            results[i] = (double)votes[i]/(double)k;
+            System.out.println("Vote " + i + ": " + results[i]);
+        }
+        
+        return results;
+    }
+    
+}
---
+++ src/uea_ml_coursework/UEA_ML_Coursework.java
@@ -0,0 +1,81 @@
+/**
+ * This class is used for generating the test cases and execuring them on the 
+ * KNN classifier.
+ */
+package uea_ml_coursework;
+
+import weka.core.Instances;
+import weka.tools.WekaTools;
+
+/**
+ * 10/04/2019
+ * @author Bijan Ghasemi Afshar (100125463)
+ */
+public class UEA_ML_Coursework {
+
+    public static void test1(String dataLocation){
+        
+        Instances allData = null;
+        
+        // Loading the data
+        try{
+            allData = WekaTools.loadData(dataLocation);
+        } catch (Exception e){
+            System.out.println("There was an issue loading the data \n" + e );
+        }
+        
+        if (allData != null){
+            
+            // Print dataset information
+//            System.out.println("Attributes: " + allData.numAttributes() 
+//                    + "\nInstances: " + allData.numInstances() + 
+//                    "\nClasses: " + allData.numClasses());
+//            double[] classDist = WekaTools.classDistribution(allData);
+//            for (int i = 0; i < classDist.length; i++){
+//                System.out.println("Class index " + i + " distribution: "
+//                        + classDist[i]);
+//            }
+            
+            // Spliting the data
+            Instances[] splitedData = WekaTools.splitData(allData, 0.3);
+            Instances trainData = splitedData[0];
+            Instances testData = splitedData[1];
+            
+            // Instantiate classifier
+            KNN oneNN = new KNN();
+            oneNN.setK(5);
+            
+            // Build the classifier using the training data
+            try{
+
+                oneNN.buildClassifier(trainData);
+
+            } catch (Exception e){
+
+                System.out.println("There was an issue building classifier\n" 
+                        + e);
+
+            }
+
+            // Test classifiers
+//            double accuracy = oneNN.classifyInstance(testData.get(0));
+//            oneNN.distributionForInstance((testData.get(0)));
+            double accuracy = WekaTools.accuracy(oneNN, testData);
+            System.out.println("The One Neares Neighbor accuracy is: " 
+                    + accuracy + "%");
+
+        }
+        
+    }
+    
+    /**
+     * The main function for testing the KNN classifier.
+     * @param args Terminal arguments passed to the program
+     */
+    public static void main(String[] args) {
+        
+        test1("./data/Height_Width_Sex.arff");
+        
+    }
+    
+}
